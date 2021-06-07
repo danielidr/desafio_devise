350075b44f667015c69fef2d333146a89481b1e1
Danielid Ramirez <danielid132@gmail.com>
2021-06-06 20:49:10 -0400

    Adding FK

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :stories
   validates_presence_of :name
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-          
+  
 end
